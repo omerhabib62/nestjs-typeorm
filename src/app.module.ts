f39473203babcde334973d1804c59ab7f6329882
Omer Habib <omer.habib62@gmail.com>
2024-02-28 10:07:56 +0500

    Setup Database  Module and configured the env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'omerHabib162@_',
-    //   database: 'nestjs_mysql_tutorial',
-    //   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   entities: [],
-    //   synchronize: true,
-    // }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin@123',
-      database: 'nestjs_typeorm_tutorial',
-      entities: [User],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'admin@123',
+    //   database: 'nestjs_typeorm_tutorial',
+    //   entities: [User],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: configService.get('DB_AUTOLOADENTITIES'),
+        synchronize: configService.get('DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
