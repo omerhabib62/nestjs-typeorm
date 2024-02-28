4e6aad5e456d93e24216f0429d718c18dc81f9f8
Omer Habib <omer.habib62@gmail.com>
2024-02-28 09:26:27 +0500

    Crud for User

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'omerHabib162@_',
+    //   database: 'nestjs_mysql_tutorial',
+    //   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   entities: [],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin@123',
+      database: 'nestjs_typeorm_tutorial',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
